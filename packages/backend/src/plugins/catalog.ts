3088fe85fba1dfff325d2e140f19379e31e2b67a
jobum97 <brian654321@naver.com>
2023-07-10 11:00:51 +0900

    [#27]feat: apply gitlab plugin

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -2,12 +2,14 @@ import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
+import {GitlabFillerProcessor} from "@immobiliarelabs/backstage-plugin-gitlab-backend";
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  builder.addProcessor(new GitlabFillerProcessor(env.config));
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
