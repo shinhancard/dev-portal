ee61d82f8314ebc70e91dd727495a3a10fad5c39
MHYOO <mhyoo@MHYOOui-Macmini.local>
2023-07-19 21:39:27 +0900

    turbo set
    실행명령어 우선 수정
    
    storybook 1st install
    
    backstage storybook 3rd commit
    
    backstage storybook open
    
    backstage storybook 2nd commit
    
    backstage storybook 1rd rollback
    
    backstage storybook 4th commit
    
    fix
    
    monorepo set complete & storybook set complete
    
    fix

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -4,16 +4,7 @@ const WebpackPluginFailBuildOnWarning = require('./webpack-plugin-fail-build-on-
 /**
  * This set of stories are the ones that we publish to backstage.io.
  */
-const BACKSTAGE_CORE_STORIES = [
-  'storybook/packages/core-components',
-  'packages/app',
-  'storybook/plugins/org',
-  'storybook/plugins/search',
-  'storybook/plugins/home',
-  'storybook/plugins/catalog-react',
-  'storybook/plugins/search-react',
-  'storybook/plugins/stack-overflow',
-];
+const BACKSTAGE_CORE_STORIES = ['plugins/home', 'plugins/home-react'];
 
 // Some configuration needs to be available directly on the exported object
 const staticConfig = {
--- storybook/plugins/search-react/README.md
+++
@@ -1,3 +0,0 @@
-# Backstage Search
-
-A search plugin library which holds functionality the [search plugin](/plugins/search/README.md) itself and other frontend plugins (e.g. [techdocs](/plugins/techdocs/README.md), [home](/plugins/home/README.md)) depend on.
