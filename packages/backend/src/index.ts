45a1631783ead71bf16891f8c2f31e3ecccbcdfc
SEO WON JIN <seowjin1060@naver.com>
2023-08-08 09:25:38 +0900

    Feature/eks pipeline (#70)
    update EKS CI / CD pipeline 
    ---------
    
    Co-authored-by: hellices <hellices@naver.com>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -21,6 +21,7 @@ import {
 } from '@backstage/backend-common';
 import { TaskScheduler } from '@backstage/backend-tasks';
 import { Config } from '@backstage/config';
+import healthcheck from './plugins/healthcheck';
 import app from './plugins/app';
 import auth from './plugins/auth';
 import catalog from './plugins/catalog';
@@ -79,6 +80,7 @@ async function main() {
   });
   const createEnv = makeCreateEnv(config);
 
+  const healthcheckEnv = useHotMemoize(module, () => createEnv('healthcheck'));
   const catalogEnv = useHotMemoize(module, () => createEnv('catalog'));
   const scaffolderEnv = useHotMemoize(module, () => createEnv('scaffolder'));
   const authEnv = useHotMemoize(module, () => createEnv('auth'));
@@ -101,6 +103,7 @@ async function main() {
 
   const service = createServiceBuilder(module)
     .loadConfig(config)
+    .addRouter('', await healthcheck(healthcheckEnv))
     .addRouter('/api', apiRouter)
     .addRouter('', await app(appEnv));
 
