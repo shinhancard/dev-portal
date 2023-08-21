ee47060f560393e7b8f1921ed10794a0181516f1
jobum97 <brian654321@naver.com>
2023-08-21 11:20:36 +0900

    [feature/generate_file_by_action]fix: 불필요한 로그 => TODO 메세지로 변경

--- packages/backend/src/providers/scaffolderRouter.ts
+++ packages/backend/src/providers/scaffolderRouter.ts
@@ -8,18 +8,16 @@ export async function scaffolderRouter(
     const { logger } = options;
     const router = Router();
 
+    //TODO: Dependencies Type list -> nexus에서 목록을 받아오든 DB에서 읽어오든 수정 필요
     router.get('/get/dependency/type', async (_, response) => {
-        logger.info('Dependencies Type list -> nexus에서 목록을 받아오든 DB에서 읽어오든 수정 필요');
         response.json(['implementation', 'runtimeOnly', 'testImplementation']);
     });
 
     router.get('/get/dependency/name', async (_, response) => {
-        logger.info('Dependencies Name list -> nexus에서 목록을 받아오든 DB에서 읽어오든 수정 필요');
         response.json(['core', 'network-starter', 'oai-starter', 'oauth-server-starter', 'core-crypto']);
     });
 
     router.get('/get/dependency/version', async (_, response) => {
-        logger.info('Dependencies Version list -> nexus에서 목록을 받아오든 DB에서 읽어오든 수정 필요');
         response.json(['0.0.1', '1.0.0', '1.1.0', '2.0.0', '2.0.1']);
     });
 
